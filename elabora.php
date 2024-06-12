2094d086b9a57e166eeaeefe0e04de7c4a7b3c54
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-06-12 19:49:32 +0200

    BONUS

--- elabora.php
+++ elabora.php
@@ -1,11 +1,47 @@
 <?php
-function generatePassword($inputLength,$password,$allowedChars ){
+// function generatePassword($inputLength,$password,$allowedChars ){
 
-    for ($i = 0; $i < $inputLength; $i++) {
-        $randomNumber = rand(0, (strlen($allowedChars)-1));
-        $randomChars = $allowedChars[$randomNumber];
-        $password .= $randomChars;
+//     for ($i = 0; $i < $inputLength; $i++) {
+//         $randomNumber = rand(0, (strlen($allowedChars)-1));
+//         $randomChars = $allowedChars[$randomNumber];
+//         $password .= $randomChars;
+//     }
+//     return $password;
+// }
+
+
+
+function generatePassword($inputLength,$password,$lettereMinuscole,$lettereMaiuscole,$simboli,$noRepeatChars){
+
+    $warehouse="";
+
+    if ($lettereMinuscole) {
+        $warehouse .= 'abcdefghijklmnopqrstuvwxyz';
     }
-    return $password;
+    if ($lettereMaiuscole) {
+        $warehouse .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if ($simboli) {
+        $warehouse .= '-_';
+    }
+
+        if(!$noRepeatChars){
+            for ($i = 0; $i < $inputLength; $i++){
+                    $randomNumber = rand(0, (strlen($warehouse)-1));
+                    $randomChars = $warehouse[$randomNumber];
+                    $password .= $randomChars;
+                }
+            return $password;  
+        }else{
+            for ($i = 0; $i < $inputLength; $i++){
+                $randomNumber = rand(0, (strlen($warehouse)-1));
+                $randomChars = $warehouse[$randomNumber];
+                if (strpos($password, $randomChars) === false) {
+                    $password .= $randomChars;
+                }
+            }
+            return $password;  
+        }
+      
 }
 ?>
--- input.php
+++ input.php
@@ -1,7 +1,13 @@
 <?php
     $inputLength = $_GET['inputLength'];
+    $lettereMinuscole = $_GET['lettereMinuscole'];
+    $lettereMaiuscole = $_GET['lettereMaiuscole'];
+    $simboli = $_GET['simboli'];
+    $noRepeatChars = $_GET['noRepeatChars'];
+
+
     $password = "";
-    $allowedChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    // $allowedChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
 
     include 'elabora.php';
 ?>
@@ -20,11 +26,19 @@
         <h2>GENERATORE DI PASSWORD</h2>
         <h4>Inserisci il numero di caratteri per la generazione della password:</h5>
         <input type="number" name="inputLength" placeholder="lunghezza password">
+        <label>
+            <input type="radio" name="lettereMinuscole" placeholder="lettereMinuscole">lettere minuscole
+            <input type="radio" name="lettereMaiuscole" placeholder="lettereMaiuscole">lettere Maiuscole
+            <input type="radio" name="simboli" placeholder="simboli">simboli
+            <input type="radio" name="noRepeatChars" placeholder="noRepeatChars">non ripetere caratteri
+        </label>
+
         <button type="submit">INVIA</button>
 
         <?php 
-        $password = generatePassword($inputLength,$password,$allowedChars);
-         ?>
+        // $password =generatePassword($inputLength,$password,$allowedChars);
+        $password =generatePassword($inputLength,$password,$lettereMinuscole,$lettereMaiuscole,$simboli,$noRepeatChars);
+        ?>
         <h3><?php
             echo "la password deve essere lungha ". $inputLength ;
         ?></h3>
